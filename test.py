c58b50e29ad6eb439fd92113c41bc6c653da6bc4
htjb <harrytjbevins@yahoo.co.uk>
2025-11-12 21:33:03 +0000

    updating readme

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 """Test script for TensorSlow library."""
 
+import matplotlib.pyplot as plt
 import numpy as np
 
-from tensorslow import math
 from tensorslow.nn.activations import sigmoid
 from tensorslow.tensor import Tensor
 
 a = Tensor(np.random.uniform(-10, 10, 50))
-bias = Tensor(0.01)
-weights = Tensor(np.random.uniform(-1, 1, (50, 5)).T)
+bias = Tensor(1)
 
-c = sigmoid(math.sum(a * weights + bias, axis=1))
-print("c data:", c.data)
+c = sigmoid(a * 2 + bias)
 c.backward()
 
 print("a grad:", a.grad)
 print("bias grad:", bias.grad)
-print("weights grad:", weights.grad)
+
+plt.plot(a.data, a.grad, ".")
+plt.title("Gradient of sigmoid at different inputs")
+plt.xlabel("Input value")
+plt.ylabel("Gradient value")
+plt.grid()
+plt.show()
