297c8902d50721380f97c5bc760dab1824b276a4
htjb <harrytjbevins@yahoo.co.uk>
2025-11-12 21:13:16 +0000

    linting the test script;

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-from tensorslow.tensor import Tensor
-from tensorslow import math
-import matplotlib.pyplot as plt
+"""Test script for TensorSlow library."""
+
 import numpy as np
+
+from tensorslow import math
+from tensorslow.nn.activations import sigmoid
+from tensorslow.tensor import Tensor
 
 a = Tensor(np.random.uniform(-10, 10, 50))
 bias = Tensor(0.01)
 weights = Tensor(np.random.uniform(-1, 1, (50, 5)).T)
 
-c = math.sigmoid(math.sum(a*weights + bias, axis=1))
+c = sigmoid(math.sum(a * weights + bias, axis=1))
 print("c data:", c.data)
 c.backward()
 
