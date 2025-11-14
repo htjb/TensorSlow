cbc498828177f9e66898e1a15706be7c2c625fd1
htjb <harrytjbevins@yahoo.co.uk>
2025-11-14 19:03:32 +0000

    testing optimisation

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 """Test script for TensorSlow library."""
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 from tensorslow.nn.loss import mse_loss
@@ -10,9 +11,25 @@ y = x * 4 + 2 + Tensor(np.random.randn(50) * 3)
 
 m = Tensor(np.random.randn())
 b = Tensor(np.random.randn())
-c = m * x + b
-diff = mse_loss(c, y)
-diff.backward()
 
-print("a grad:", m.grad)
-print("bias grad:", b.grad)
+learning_rate = 0.001
+
+loss = []
+for i in range(1000):
+    c = m * x + b
+    loss = mse_loss(c, y)
+    loss.backward()
+    m.data -= learning_rate * m.grad
+    b.data -= learning_rate * b.grad
+    if i % 100 == 0:
+        print(f"Iteration {i}: Loss = {loss.data}")
+    m.grad = np.zeros_like(m.data)
+    b.grad = np.zeros_like(b.data)
+    x.grad = np.zeros_like(x.data)
+    y.grad = np.zeros_like(y.data)
+    loss.grad = np.zeros_like(loss.data)
+
+plt.plot(x.data, y.data, "o", label="Data")
+plt.plot(x.data, (m.data * x.data + b.data), label="Fitted Line")
+plt.legend()
+plt.show()
